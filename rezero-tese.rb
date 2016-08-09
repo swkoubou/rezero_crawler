934fc2938d852df05456cc6be4e64e0c4a445c1f
NISHI3 <ryonishi3@gmail.com>
2016-08-09 22:02:21 +0900

    狩り

---
+++ rezero-tese.rb
@@ -0,0 +1,22 @@
+require 'nokogiri'
+require 'anemone'
+require 'kconv'
+
+url = "http://www.amazon.co.jp/dp/B01DRX3DS2"
+
+opts = {
+    depth_limit: 0,
+    :delay => 1
+}
+
+Anemone.crawl(url, opts) do |anemone|
+  anemone.on_every_page do |page|
+    doc = Nokogiri::HTML.parse(page.body.toutf8)
+    
+    title = doc.xpath("/html/body/div[2]/div[4]/div[6]/div[1]/div/h1/span").text
+    price = doc.xpath("/html/body/div[2]/div[4]/div[6]/div[5]/div/table/tbody/tr[2]/td[2]").text
+    puts title + price
+  end
+end
+
+
