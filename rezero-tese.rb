b224c206b38a2d97c7068ac2037e4ddd19a6f6ca
NISHI3 <ryonishi3@gmail.com>
2016-08-10 11:34:12 +0900

    少し変更

--- rezero-tese.rb
+++ rezero-tese.rb
@@ -13,9 +13,9 @@ Anemone.crawl(url, opts) do |anemone|
   anemone.on_every_page do |page|
     doc = Nokogiri::HTML.parse(page.body.toutf8)
     
-    #title = doc.xpath("/html/body/div[2]/div[4]/div[6]/div[1]/div/h1/span").text.strip
+    title = doc.xpath("/html/body/div[2]/div[4]/div[6]/div[1]/div/h1/span").text.strip
     price = doc.xpath("/html/body/div[2]/div[4]/div[6]/div[5]/div/table/tbody/tr[2]/td[2]/span[1]").text
-    #puts title + price
+    puts title + price
     puts price
   end
 end
